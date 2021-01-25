84b0b86ab2c6de54b5e31b5d8497735ce08c34fe
JudahSan <baraka.judah@gmail.com>
2021-01-25 03:47:53 +0300

    Driver code update

--- JudahSan.py
+++ JudahSan.py
@@ -1,31 +1,33 @@
 
 #!/usr/bin/env python3
 
-def getX (a, b, c):
+def getX(a,b,c):
 
 	# Calculate the value of second part
-	d = c * 1.0 - (b * b / (4.0 * a));
+	d = c * 1.0 - (b * b / (4.0 * a))
 
 	# Print the values
 	if (a > 0) :
 
 		# Open upward parabola function
-		print("Maxvalue =", "Infinity");
+		print("Maxvalue =", "Infinity")
 
 
 	elif (a < 0) :
 
 		# Open downward parabola function
-		print("Maxvalue = ", d);
+		print("Maxvalue = ", d)
 
 
 	else :
 
 		# If a=0 then it is not a quadratic function
-		print("Not a quadratic function");
+		print("Not a quadratic function")
 
 
 if __name__ == "__main__" :
-	a = -1; b = 3; c = -2;
+	a = int(input("Enter the coefficients of a: "))
+	b = int(input("Enter the coefficients of b: "))
+	c = int(input("Enter the coefficients of c: "))
 
-	getX(a, b, c);
+	getX(a, b, c)
