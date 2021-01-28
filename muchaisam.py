51b0870bb9e0655a3f870f96a15167a04b8fba21
muchaisam <samson.muchai@strathmore.edu>
2021-01-28 09:51:43 +0300

    function

---
+++ muchaisam.py
@@ -0,0 +1,5 @@
+# Given a, b, c for a quadratric expression ax2 + bx + c = 0.
+#  Write a function getX that returns the larger of the values for X, 
+#  i.e. given x1 = -2 and x2 = 5, getX should return 5.
+
+max([-2, 5])
