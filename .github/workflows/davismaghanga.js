ea7338183d9d4d73f5393b3fe03ea0128f390253
devosuka <dkirughamio@gmail.com>
2021-01-28 20:00:36 +0300

    second commit

--- .github/workflows/davismaghanga.js
+++
@@ -1,7 +0,0 @@
-function getX(a, b, c) {
-    var x1 = (-1 * b + Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-    var x2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - (4 * a * c))) / (2 * a);
-    return Math.max(x1,x2);
-}
-
-getX(1,2,-3); //returns 1
