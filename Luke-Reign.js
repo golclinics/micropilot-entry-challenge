fa622dc1d37002bec77714318cef7292e3e5470c
Luke Reign <lkibiku@gmail.com>
2021-01-28 10:23:02 +0300

    luke-reign golclinic entry challenge

---
+++ Luke-Reign.js
@@ -0,0 +1,10 @@
+// Function getx() that returns the larger of the values for x
+function getx(x1,x2){
+    if (x1>x2) {
+        return x1;
+    } else {
+        return x2;
+    }
+}
+// Prints the larger value of x in the console
+console.log(getx(-2,5)) 
