5357aa624eb0f356b8ace557b36bc5d3dfc531b6
makenasandra <makenawachira@gmail.com>
2021-01-28 13:13:23 +0300

    testing code

---
+++ .github/makena_sandra.py
@@ -0,0 +1,25 @@
+from math import sqrt
+
+def getX(a, b, c):
+
+    s_root = (b ** 2) - (4 * a * c)
+
+    if s_root > 0:
+        x1 = (((-b) + sqrt(s_root)) / (2 * a))
+        x2 = (((-b) - sqrt(s_root)) / (2 * a))
+        print("For the given values the larger value of x is {}".format(max(x1, x2)))
+
+    elif s_root == 0:
+        x = (-b) / 2 * a
+        print("For the given values x is {} ", x)
+
+    else:
+        print("For the given values there are no roots")
+        exit()
+
+    return
+
+if __name__ == '__main__':
+    getX(5, 20, 10)
+
+
