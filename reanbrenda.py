21679ae7621ab09a7dbc55ae831d86d6f26288be
reanbrenda <mukindiabrenda67@gmail.com>
2021-01-30 01:34:33 +0300

    second commit

--- reanbrenda.py
+++ reanbrenda.py
@@ -1,14 +1,16 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import math
 
 
 def getx(a,b,c):
     squarev = b * b - (4 * a * c)
-    x1 = (-b - math.sqrt(squarev)) / (2 * a)
-    x2 = (-b + math.sqrt(squarev)) / (2 * a)
+    if (a == 0):
+        return None
+        # calculating the square root
+    squarev = b * b - (4 * a * c)
+    if squarev >= 0:
+        x1 = (-b - math.sqrt(squarev)) / (2 * a)
+        x2 = (-b + math.sqrt(squarev)) / (2 * a)
     if x1 < x2:
         print(x2)
     else:
