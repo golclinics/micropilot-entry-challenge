267016f5b47a1b8ab77e6c0de2dd4e7111b3225e
mariamiah <mariah.kakande@gmail.com>
2021-01-29 13:47:11 +0300

    update return value

--- mariamiah.js
+++ mariamiah.js
@@ -3,7 +3,7 @@ const getX = (a, b, c) => {
     if(discriminant > 0) {
         const x1 = ((-1 * b) + Math.sqrt(discriminant)) / (2 * a)
         const x2 = ((-1 * b) - Math.sqrt(discriminant)) / (2 * a)
-        return (x1, x2)? Math.max(x1, x2): null
+        return Math.max(x1, x2)
     } else {
         return null
     }
