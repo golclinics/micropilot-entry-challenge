0c293570217b4d484b077c6d80899844070bc462
dmukuna <danielmukuna.k@outlook.com>
2021-01-29 18:10:11 +0300

    Added test cases

--- dmukuna.py
+++ dmukuna.py
@@ -2,14 +2,18 @@ from math import sqrt, pow
 
 
 def getX(a, b, c):
-    x1 = (-b + sqrt(pow(b, 2) - (4 * a * c)))/(2*a)
-    x2 = (-b - sqrt(pow(b, 2) - (4 * a * c)))/(2*a)
+    x1 = (-b + sqrt(pow(b, 2) - (4 * a * c)))/(2 * a)
+    x2 = (-b - sqrt(pow(b, 2) - (4 * a * c)))/(2 * a)
 
-    x = 0
-    if x1 >= x2:
-        x = x1
-    else:
-        x = x2
+    return round(max(x1, x2), 2)
 
-    return x
 
+def test():
+    assert getX(1, -2, -24) == 6
+    assert getX(1, -4, 4) == 2
+    assert getX(1, 1.4, 0.45) == -0.5
+
+
+if __name__ == "__main__":
+    test()
+    print("Tests passed")
