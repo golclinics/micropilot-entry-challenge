1e68f1b1a21ee98f9e6a670a7a8c3de281a034e8
ero <eric.ndira@gmail.com>
2021-02-01 23:51:17 +0300

    added main function to test solution

--- erodrago.py
+++ erodrago.py
@@ -16,3 +16,12 @@ def getX(a, b, c):
     x1, x2 = compute_roots(a, b, c)
 
     return max(x1.real, x2.real)
+
+
+if __name__ == "__main__":
+
+    a = float(input("Enter value of a \n"))
+    b = float(input("Enter value of b \n"))
+    c = float(input("Enter value of c \n"))
+
+    print(getX(a, b, c))
