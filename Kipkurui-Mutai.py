76ecb21c00de4e17b38c7163d6ccf674f50177df
Kipkurui Mutai <hpetes69@gmail.com>
2021-02-02 22:14:12 +0300

    Update Kipkurui-Mutai.py

--- Kipkurui-Mutai.py
+++ Kipkurui-Mutai.py
@@ -17,7 +17,6 @@ def getX(a,b,c):
         x1 = (-b+math.sqrt((b**2)-(4*(a*c))))/(2*a) ; x2 = (-b-math.sqrt((b**2)-(4*(a*c))))/(2*a)
         if (x1 > x2): print('x1:', x1)
         else: print('x2:', x2)
-    except ValueError:
-        print("Impossible, No square root of a negative number")
+    except ValueError: print("Impossible, No square root of a negative number")
 
 getX(2, 101, 14)
