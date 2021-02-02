b9d69aab880dfd31eeef308304398ec77fdccebb
Brian Kariu <48643861+Brian-Kariu@users.noreply.github.com>
2021-02-02 13:42:30 +0300

    Code for the challenge

---
+++ Brian-Kariu.py
@@ -0,0 +1,17 @@
+import math
+def getX(a,b,c):
+    try:
+        x1 = (-b - math.sqrt(b*b - 4*a*c))/2*a
+        x2 = (-b + math.sqrt(b*b - 4*a*c))/2*a
+        if x2 > x1:
+            return x2
+        else:
+            return x1
+    except ValueError:
+        print("Impossible, Can't calculate square root of a negative number")
+
+a = int(input("Enter value of a: "))
+b = int(input("Enter value of b: "))
+c = int(input("Enter value of c: "))
+
+print(getX(a,b,c))
