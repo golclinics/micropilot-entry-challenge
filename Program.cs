a03091311fab8018b734048955c505235979935c
Simiyu Robert N <salmonrobertsimiyu@gmail.com>
2021-02-05 23:24:23 +0300

    Implemented solution for the quadratic equation

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Quadratic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine(getX(1,5,6));
+        }
+
+        public static double getX(double a=1, double b=1, double c=0)
+        {
+            double x1, x2;
+            
+            double firstPart =Math.Sqrt((b * b)-(4 * a * c));
+
+            b *= -1;
+
+            x1 = ((b) - firstPart) / (2 * a);
+
+            x2 = ((b) + firstPart) / (2 * a);
+
+            return Math.Max(x1, x2);
+        }
+    }
+}
