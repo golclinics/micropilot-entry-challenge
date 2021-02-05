4ec6fd0bbe100137383771989d0ffdf2d75aeca2
Jan Gichuru Sultan <gichu116@gmail.com>
2021-02-05 20:39:03 +0300

    Update warspan.py

--- warspan.py
+++ warspan.py
@@ -1,12 +1,13 @@
 import math
 
-a = int(input("Enter a: "))
-b = int(input("Enter b: "))
-c = int(input("Enter c: "))
+def getX(a, b, c):
+    dis = math.sqrt((b**2) - (4*a*c))
+    l = (-b + dis)/(2*a)
+    h = (-b - dis)/(2*a)
+    return l if l > h else h
 
-dis = math.sqrt((b**2 - 4*a*c))
-
-l = (-b + dis)/(2*a)
-h = (-b - dis)/(2*a)
-
-print(f'x = {l if l > h else h}') 
+if __name__ == '__main__':
+    a = float(input("Enter a: "))
+    b = float(input("Enter b: "))
+    c = float(input("Enter c: "))
+    print(getX(a, b, c))
