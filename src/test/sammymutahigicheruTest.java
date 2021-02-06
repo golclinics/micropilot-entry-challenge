cbdfd3b363ca9b631d4125b867fd75865fa3a8d1
Sammy Mutahi <sammymutahi12@gmail.com>
2021-02-06 13:41:42 +0300

    add test for complex roots where determinant is less than 0 and make it pass

--- src/test/sammymutahigicheruTest.java
+++ src/test/sammymutahigicheruTest.java
@@ -8,7 +8,7 @@ import org.junit.Test;
 * */
 public class sammymutahigicheruTest{
     private sammymutahigicheru quadratic;
-    private final double DELTA = 1e-15;
+    private final double DELTA = 1e-15; //handles off by for double values
     @Before
     public void setUp(){
         quadratic = new sammymutahigicheru();
@@ -16,5 +16,14 @@ public class sammymutahigicheruTest{
     @Test
     public void firstCaseDetGreaterThanZero_ReturnsMaxRealRoot(){
         Assert.assertEquals(2,quadratic.getX(1,10,-24),DELTA);
+    }
+    @Test
+    public void secondCaseDeterminantEqualsZero_ReturnGreaterOfTheRoots(){
+        Assert.assertEquals(8,quadratic.getX(1,-15,56),DELTA);
+    }
+    //-2.780776,-0.7192235935955849
+    @Test
+    public void thirdCaseDeterminantLessThanZero_ReturnsGreaterOfTheTwoNegativeRealNumbers(){
+        Assert.assertEquals(-0.7192235935955849,quadratic.getX(2,7,4),DELTA);
     }
 }
