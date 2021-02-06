d2a088c9be56eaa5625211fa358e001a820d7b6d
Jama Mohamed <jama3137@gmail.com>
2021-02-06 15:19:46 +0300

    Added docs

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GOL Clinics Entry Challenge 
 
 Entry challenges for the GOL clinics 
+
+<img src = "https://github.com/jama5262/micropilot-entry-challenge/blob/master/image.gif">
--- jama5262.js
+++ jama5262.js
@@ -3,12 +3,20 @@ const cli = require('./cli')
 const jest = require('jest')
 
 let start = async () => {
+    // Start cli interation
+    // Promp user to choose either get x of run tests
     let choice = await cli.getUserChoice()
+    
     if (choice === "Run Tests") {
+        // Run tests
         jest.run([])
         return
     }
+
+    // Prompt user to input nums a, b and c
     let num = await cli.getNums()
+
+    // Output the answer
     console.log(`Larger value of X is -> ${getX(...num)}`);
 }
 
