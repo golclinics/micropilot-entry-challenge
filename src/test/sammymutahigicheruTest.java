d97632545d120a8f3cee23fc00fd82c270f8267d
Sammy Mutahi <sammymutahi12@gmail.com>
2021-02-06 13:29:36 +0300

    pass the first test case where determinant is greater than 0

--- src/test/sammymutahigicheruTest.java
+++ src/test/sammymutahigicheruTest.java
@@ -1,8 +1,20 @@
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 /*
-* Makes sure the algorthm does as expected
+* Makes sure the algorithm does as expected
 * */
 public class sammymutahigicheruTest{
-
+    private sammymutahigicheru quadratic;
+    private final double DELTA = 1e-15;
+    @Before
+    public void setUp(){
+        quadratic = new sammymutahigicheru();
+    }
+    @Test
+    public void firstCaseDetGreaterThanZero_ReturnsMaxRealRoot(){
+        Assert.assertEquals(2,quadratic.getX(1,10,-24),DELTA);
+    }
 }
