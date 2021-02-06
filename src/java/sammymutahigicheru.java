87f04f0ca1d52649710ea96c4f258e1ca2398c99
Sammy Mutahi <sammymutahi12@gmail.com>
2021-02-06 13:46:29 +0300

    verified the algorithm does as expected by testing all edge cases

--- src/java/sammymutahigicheru.java
+++ src/java/sammymutahigicheru.java
@@ -1,14 +1,14 @@
 
 /*
-* Given a, b, c for a quadratric expression ax2 + bx + c = 0.
-*  Write a function getX that returns the larger of the values for X, i.e. if x1 = -2 and x2 = 5, getX should return 5.
-*
-*
-* */
+ * Given a, b, c for a quadratric expression ax2 + bx + c = 0.
+ *  Write a function getX that returns the larger of the values for X, i.e. if x1 = -2 and x2 = 5, getX should return 5.
+ *
+ *
+ * */
 
 public class sammymutahigicheru {
 
-    double getX(double a,double b,double c){
+    double getX(double a, double b, double c) {
 
         //roots
         double x2 = 0;
@@ -18,9 +18,9 @@ public class sammymutahigicheru {
         double determinant = b * b - 4 * a * c;
 
         /*
-        * First Test Case( det > 0)
-        *
-        * */
+         * First Test Case( det > 0)
+         *
+         * */
         if (determinant > 0) {
 
             // two real and distinct roots
@@ -29,8 +29,8 @@ public class sammymutahigicheru {
 
         }
         /*
-        * Second Test Case (det == 0)
-        * */
+         * Second Test Case (det == 0)
+         * */
         else if (determinant == 0) {
 
             // x1 and x2 are equal
@@ -39,8 +39,8 @@ public class sammymutahigicheru {
             x1 = x2 = -b / (2 * a);
         }
         /*
-        * Third Test Case (det < 0)
-        * */
+         * Third Test Case (det < 0)
+         * */
         else {
 
             // x1 and x2 are complex numbers and distinct
