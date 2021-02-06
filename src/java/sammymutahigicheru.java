a1ec467ef5fe094fbdee5fe7b3842359e19af5eb
Sammy Mutahi <sammymutahi12@gmail.com>
2021-02-06 12:54:54 +0300

    add problem description

--- src/java/sammymutahigicheru.java
+++ src/java/sammymutahigicheru.java
@@ -1,3 +1,11 @@
+
+/*
+* Given a, b, c for a quadratric expression ax2 + bx + c = 0.
+*  Write a function getX that returns the larger of the values for X, i.e. if x1 = -2 and x2 = 5, getX should return 5.
+*
+*
+* */
+
 public class sammymutahigicheru {
 
 }
