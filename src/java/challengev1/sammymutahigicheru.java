11981f2c02e10333961e77a81c7fa5b3a5bb0767
Sammy Mutahi <sammymutahi12@gmail.com>
2021-02-08 13:44:58 +0300

    put the class countzeros to sammymutahigicheru directory

--- src/java/challengev1/sammymutahigicheru.java
+++ src/java/challengev1/sammymutahigicheru.java
@@ -1,4 +1,4 @@
-
+package challengev1;
 /*
  * Given a, b, c for a quadratric expression ax2 + bx + c = 0.
  *  Write a function getX that returns the larger of the values for X, i.e. if x1 = -2 and x2 = 5, getX should return 5.
--- src/java/sammymutahigicheru/CountZeros.java
+++ src/java/sammymutahigicheru/CountZeros.java
@@ -1,6 +1,7 @@
+package sammymutahigicheru;
 
 /*
-* Write a function CountZeros(A) that takes in an array of integers A, and returns the number of 0's in that array.
+* Write a function challengev1.sammymutahigicheru.CountZeros(A) that takes in an array of integers A, and returns the number of 0's in that array.
 * For example, given [1, 0, 5, 6, 0, 2], the function/method should return 2.
 * */
 public class CountZeros {
