47b5475d18b7352e1ca58bbf419f1738fb6c507f
Fela Fela <olowofelayinka@gmail.com>
2021-02-20 04:26:59 +0100

    challenge completed

---
+++ olowoyinka/CountZeros.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Clincs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the number array digit");
+
+            int ArrayLength = Convert.ToInt32(Console.ReadLine());
+
+            int[] arrnumber = new int[ArrayLength];
+
+            for (int i = 0; i < ArrayLength; i++)
+            {
+                Console.WriteLine($"Enter the {i} digit array");
+
+                arrnumber[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int numberofO = CountZeros(arrnumber);
+
+            Console.WriteLine($"The number of O's in the array is {numberofO}");
+        }
+
+        static int CountZeros(int[] A)
+        {
+            int countA = 0;
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                if (A[i] == 0)
+                {
+                    countA++;
+                }
+            }
+
+            return countA;
+        }
+    }
+}
