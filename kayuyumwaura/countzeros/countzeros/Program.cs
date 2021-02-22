35529388f33ccb01ddd26d0e1364844e08f14496
kayuyu.mwaura <kayuyu.mwaura@tovutigroup.com>
2021-02-22 12:56:00 +0300

    initial commit

---
+++ kayuyumwaura/countzeros/countzeros/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace countzeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] A = { 1, 0, 5, 6, 0, 2 };
+            int count = 0;
+
+            for (int j = 0; j < (A.Length - 1); j++)
+            {
+                if (A[j] == 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+            Console.ReadKey();
+        }
+    }
+}
