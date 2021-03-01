10504f1a240a246715fd4fa8993867fe9b7f8893
Edwin Cheruiyot <edukipchirchir@gmail.com>
2021-03-01 22:01:40 +0300

    Add testdata

--- EdwinCheruiyot/count_zeros.py
+++ EdwinCheruiyot/count_zeros.py
@@ -1,13 +1,10 @@
-count=0
-def CountZeros(A):
-    for i in A:
-        if i == 0:
-            count=+1
-            print(count)
-          
-
-
-
-A=[1, 0, 5, 6, 0, 2]
-CountZeros(A)
+def CountZeros(A): # Create a function called A
+    count=0 # Initalize the count value
+    for i in A: # Create a for loop to get values in the array
+        if i == 0: # check if value matches Zero
+            count+=1 # count the number of times the value occurs
+    print("Number of times Zero occurs: ",count)  #Resulting output printed on console
+        
+A=[1, 0, 0, 5, 6, 0, 2] # an example array
+CountZeros(A) # Function CountZero is called
 
