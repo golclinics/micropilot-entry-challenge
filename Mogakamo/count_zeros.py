c4dcd876ad45e91876a1a6150e4f7b4eb1c0920c
Mogakamo <mogaka.amo254@gmail.com>
2021-07-27 17:04:27 +0300

    commit

--- Mogakamo/count_zeros.py
+++ Mogakamo/count_zeros.py
@@ -3,4 +3,4 @@ def countZeros(list_of_int: List[int]) -> int:
      return list_of_int.count(0)
 
 assert countZeros(([1,0,5,6,0,2,1]))== 2, "Number of zeros should be 2"
-assert countZeros(([0,0,0,0,0,0,0]))== 6, "Number of zeros should be 7"
+assert countZeros(([0,0,0,0,0,0,0]))== 7, "Number of zeros should be 7"
