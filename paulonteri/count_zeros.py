00668db0eaed8d520afc3b31f2ef83a641e388e8
Paul Onteri <onteripaul@gmail.com>
2021-08-29 10:58:08 +0300

    Create count_zeros.py

---
+++ paulonteri/count_zeros.py
@@ -0,0 +1,12 @@
+""" 
+Write a function CountZeros(A) that takes in an array of integers A, 
+and returns the number of 0's in that array. For example, given [1, 0, 5, 6, 0, 2], the function/method should return 2.
+"""
+
+
+def CountZeros(A):
+    count = 0
+    for num in A:
+        if num == 0:
+            count += 1
+    return count
