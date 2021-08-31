ec8f3aa9b8ed22821bd308669abe87cb26370178
victor mwenda <victormwenda30@gmail.com>
2021-08-31 22:35:59 +0300

    count_zero.py

---
+++ victoriousvictor42/count_zeros.py
@@ -0,0 +1,9 @@
+# Creating the function CountZeros(A)
+def count_zeros(A):
+    return A.count(0)
+    
+# Creating an array and storing it in the variable, A.
+A = [1, 0, 5, 6, 0, 2]
+# Running the function
+print(count_zeros(A))
+
