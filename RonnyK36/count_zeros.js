e58cf60d4060a94dbf7fd481f532d2f5f3db6fc8
RonnyK36 <ronnykelvyne3@gmail.com>
2021-09-05 21:17:32 +0300

    Comments added

--- RonnyK36/count_zeros.js
+++ RonnyK36/count_zeros.js
@@ -1,8 +1,11 @@
+// a function to count the occurence of zeros(0) in a list
+
 let myList = [1, 4, 9, 0, 6, 2, 8, 0, 6, 4, 0, 3, 7];
 
 let counter = 0;
 
-const CountZeros = (myList) => {
+// function taking in myList and counting number of zeros
+const countZeros = (myList) => {
     for (let i = 0; i < myList.length; i++) {
         if (myList[i] === 0) {
             counter += 1;
@@ -10,4 +13,6 @@ const CountZeros = (myList) => {
     }
     return counter;
 };
-console.log(CountZeros(myList));
+
+// show the result
+console.log(countZeros(myList));
