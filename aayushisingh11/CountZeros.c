98fb093e2ba5d981187fd881fe52e573a32267f8
Aayushi Singh <75681857+aayushisingh11@users.noreply.github.com>
2021-09-21 13:37:18 +0530

    Updated

--- aayushisingh11/CountZeros.c
+++ aayushisingh11/CountZeros.c
@@ -1,37 +1,23 @@
 #include <stdio.h>
-int CountZeros(int arr[], int low, int high)
+
+void main()
 {
-    if (high >= low)
+    int arr[50], n, i, positive = 0, negative = 0, zero = 0;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter array elements-\n");
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (i = 0; i < n; i++)
     {
-        
-        int mid = low + (high - low)/2;
-        if (( mid == 0 || arr[mid-1] == 1) && arr[mid] == 0)
-            return mid;
-  
-        if (arr[mid] == 1) 
-            return CountZeros(arr, (mid + 1), high);
-        else  
-            return CountZeros(arr, low, (mid -1));
+        if (arr[i] > 0)
+            positive++;
+        else if (arr[i] < 0)
+            negative++;
+        else
+            zero++;
     }
-    return -1;
-}
-  
-int countZeroes(int arr[], int n)
-{
- 
-    int first = CountZeros(arr, 0, n-1);
-  
-    if (first == -1)
-        return 0;
-  
-    return (n - first);
-}
-  
-/* Driver program to check above functions */
-int main()
-{
-    int arr[] =   {1, 1, 1, 0, 0, 0, 0, 0, 0};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    printf("Count of zeroes is %d", countZeroes(arr, n));
-    return 0;
-}
+    printf("Number of zeros: %d", zero);
