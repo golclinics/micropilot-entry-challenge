e2e13a081168c1b138ff1ee5b3b40a935f18a4c3
Brian Okanga <briannokanga@gmail.com>
2021-09-22 15:53:53 +0300

    first commit

---
+++ brianokanga/CountZeros.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CountZeros
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = new Counter();
+            int[] numArray = { 1, 0, 5, 6, 0, 2, 0, 4, 4, 0 };
+
+            counter.CountZeros(numArray);
+        }  
+    }
+
+    public class Counter
+    {
+        public void CountZeros(int[] numArray)
+        {
+            int count = 0;
+            for (int i = 0; i < numArray.Length; i++)
+            {
+                if (numArray[i] == 0)
+                    count += 1;
+            }
+            Console.WriteLine($"The number zeros in the array  is {count}");
+        }
+    }
+}
