927769809eded0bc16310b62c968a3b4a545d2b0
Leo-Bikuri <leo.bikuri@strathmore.edu>
2021-09-22 14:05:30 +0300

    Create countZeros.java

---
+++ Leo-Bikuri/countZeros.java
@@ -0,0 +1,14 @@
+public class MyClass {
+    
+    public int CountZeros(int A[]){
+        int counter = 0;
+        for(int x = 0;x < A.length; x++){
+            if(A[x]==0){
+                counter++;
+            }
+        }
+        return counter;
+    }
+    public static void main(String args[]) {
+    }
+}
