d3a6bd4c8dd0771f04b350a29f7616828846e2a2
Duncan Moiyo <41007172+saretoduncan@users.noreply.github.com>
2021-09-23 09:34:32 +0300

    add comments where necessary

--- saretoduncan/Main.java
+++ saretoduncan/Main.java
@@ -1,12 +1,12 @@
 
 public class Main{
     public static void main(String[] args){
-        int[] numArr = {1, 0, 5, 6, 0, 2,0,0,0};
-        int noOfZeros=countZeros(numArr);
+        int[] numArr = {1, 0, 5, 6, 0, 2,0,0,0};// test array
+        int noOfZeros=countZeros(numArr);// call counter method
         System.out.println(noOfZeros);
 
     }
-    public static int countZeros(int[] num){
+    public static int countZeros(int[] num){//counter method
             int results=0;
                 for(int i=0; i<num.length;i++){
                     if(num[i]==0){
