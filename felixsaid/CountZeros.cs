4bff8015f391f227fee9591dfedf7a12f6cd3e60
Felix Kiamba <kiambafelix@tutanota.com>
2021-09-24 09:17:21 +0300

    created countzeros class

---
+++ felixsaid/CountZeros.cs
@@ -0,0 +1,14 @@
+public class int CountZeros(int[] A)
+{
+    int zerosCounter = 0;
+
+    foreach(int item in A)
+    {
+        if(item == 0)
+        {
+            zerosCounter++;
+        }
+
+        return zerosCounter;
+    }
+}
