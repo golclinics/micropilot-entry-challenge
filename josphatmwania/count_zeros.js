1ef28e73901de686973e9311217cc4550532ec42
josphatmwania <josphatmwania237@gmail.com>
2021-09-25 13:58:37 +0300

    Fix return value

--- josphatmwania/count_zeros.js
+++ josphatmwania/count_zeros.js
@@ -4,6 +4,7 @@
 // Arrays = [1, 0, 5, 0, 6, 0, 8, 0,2]
 
 function CountZeros(A) {
+
     var zeros = [];
     for(var i = 0; i < A.length; i ++){
     	if(A[i] === 0) {
@@ -11,6 +12,7 @@ function CountZeros(A) {
         }
     }
     console.log(zeros.length);
+    console.log(CountZeros);
 }
 
 CountZeros([1, 0, 5, 0, 6, 0, 8, 0,2])
