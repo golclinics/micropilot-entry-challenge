1d51ea5829c3d868366395ff59b01f3591966460
stevemunene <stevemune6@gmail.com>
2021-09-27 07:09:23 +0300

    Count Zeros in provides Array A

--- stevemunene/count_zeros.py
+++ stevemunene/count_zeros.py
@@ -2,6 +2,7 @@ def main():
     A = [1, 0, 5, 6, 0, 2]
     print('0 has occured {} times'.format(CountZeros(A)))
 
+#count function to count number of 0's in array A
 def CountZeros(A):
     return A.count(0)
 
