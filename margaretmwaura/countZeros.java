802813843199eb8b03dc9d1d9b053603978d4197
margaretmwaura <mwauramargaret1@gmail.com>
2021-10-07 07:42:00 +0300

    code cleanup

--- margaretmwaura/countZeros.java
+++ margaretmwaura/countZeros.java
@@ -1,8 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         
-        System.out.println("Hello World!");
-        
         int[] values = new int[]{1, 0, 5, 6, 0, 2, 0, 0, 0 , 0};
         int result = countZeros(values);
         System.out.print(result);
