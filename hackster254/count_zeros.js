1000fc741cb6d7880002b18dd7672076ef7349e6
karira <43937812+hackster254@users.noreply.github.com>
2021-10-09 12:32:32 +0300

    Update count_zeros.js

--- hackster254/count_zeros.js
+++ hackster254/count_zeros.js
@@ -1,1 +1,14 @@
+let A = [];
+let i;
+let counter = 0;
 
+function CountZeros(A) {
+    for (i = 0; i < A.length; i++) {
+        if (A[i] === 0) {
+            counter = counter + 1;
+        }
+    }
+    return counter;
+
+}
+console.log(CountZeros([4, 5, 6, 0, 1, 0, 7, 0, 0]));
