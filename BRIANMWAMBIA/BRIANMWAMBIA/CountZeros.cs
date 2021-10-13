8e5d804489265890368056f3e3ea2effe263d412
brian <brianmwambia3@gmail.com>
2021-10-13 17:59:48 +0300

    counted zeros

---
+++ BRIANMWAMBIA/BRIANMWAMBIA/CountZeros.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BRIANMWAMBIA
+{
+    public class CountZeros
+    {
+       public static int countZeros(int[] num)
+        {
+
+            int count = 0;
+            //loop through the array checking for zero
+
+            for (int i = 0; i<num.Length; i++)
+            {
+                //increment count every time this returns true
+                if (num[i] == 0)
+                {
+                    count +=1;
+                }
+
+            } Console.WriteLine(count);
+            return count;
+        }
+
+    }
+}
---
+++ BRIANMWAMBIA/BRIANMWAMBIA/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BRIANMWAMBIA
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            var num = new int[] {1,0,0,0,0,1};
+            CountZeros.countZeros(num);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
