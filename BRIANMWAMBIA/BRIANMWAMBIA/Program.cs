09f5bedb443abba4b06ebc84cf1d1038b91bc9d9
brian <brianmwambia3@gmail.com>
2021-10-13 18:01:56 +0300

    working

--- BRIANMWAMBIA/BRIANMWAMBIA/Program.cs
+++ BRIANMWAMBIA/BRIANMWAMBIA/Program.cs
@@ -7,8 +7,8 @@ namespace BRIANMWAMBIA
         static void Main(string[] args)
 
         {
-            var num = new int[] {1,0,0,0,0,1};
-            CountZeros.countZeros(num);
+            var num = new int[] {1,0,0,0,0,1}; //Input
+            CountZeros.countZeros(num); //output 4
             Console.WriteLine("Hello World!");
         }
     }
