d2a1b7dfde69bcc885daeda711a9837d65696fe4
anthonykimani <anthonykimani@student.moringaschool.com>
2022-07-02 19:49:19 +0300

    Updates the repository with unit tests using jest

---
+++ anthonykimani/index.test.js
@@ -0,0 +1,10 @@
+const CountZeros = require('./index');
+
+test("a function CountZeros() that takes in an array of integers, and returns the number of 0's in that array",()=>{
+    const A=[1, 0, 5, 6, 0, 2];
+    const number = 0;
+    expect(CountZeros(A))toStrictEqual(A.filter(element=>{
+        let filterNumber = element===number;
+        return filterNumber.length;
+    }))
+})
