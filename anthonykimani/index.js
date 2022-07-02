e110502181a270052a6620199b27ac1656210214
anthonykimani <anthonykimani@student.moringaschool.com>
2022-07-02 20:54:25 +0300

    Adds a detailed description on how to solve the problem

--- anthonykimani/index.js
+++ anthonykimani/index.js
@@ -1,3 +1,10 @@
+
+//PSEUDOCODE
+/* The function takes an array of intergers and uses the filter method to filter out elements,
+that are not equal to the number zero , using a strict equality operator .Then uses the length method to get the number
+of 0's in the array and returns the output.*/
+
+//a function CountZeros() that takes in an array of integers, and returns the number of 0's in that array
 const CountZeros =(A)=>{
     const filteredArray = A.filter(element=>element === 0);
     return filteredArray.length;
